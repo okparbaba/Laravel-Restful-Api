08d97a974412a71cc181edcc2496430ed2673682
yannaing-lynn <rghtbossy@gmail.com>
2019-06-12 15:15:36 +0630

    database seeding

--- database/factories/ReviewsFactory.php
+++ database/factories/ReviewsFactory.php
@@ -3,10 +3,16 @@
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
 use App\Model\Reviews;
+use App\Model\Product;
 use Faker\Generator as Faker;
 
 $factory->define(Reviews::class, function (Faker $faker) {
     return [
-        //
+        'product_id'=>function(){
+            return Product::all()->random();
+        },
+        'customer'=>$faker->name,
+        'review'=>$faker->paragraph,
+        'star'=>$faker->numberBetween(0,5)
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Model\Product;
+use App\Model\Reviews;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(App\Model\Product::class,50)->create();
+        factory(App\Model\Reviews::class,300)->create();
+
     }
 }
